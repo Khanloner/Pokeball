d78431af3ce69e706b9308eb587449eb56970626
Sahil khan <90113862+Khanloner@users.noreply.github.com>
2022-09-30 15:18:03 +0530

    Add files via upload

---
+++ pokeball.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+canvas = Canvas(window,height=500,width=500)
+
+canvas.create_arc(0,0,500,500,fill="red",extent=180,width=10)
+canvas.create_arc(0,0,500,500,fill="white",start=180,extent=180,width=10)
+canvas.create_oval(190,190,310,310,fill="white",width=10)
+
+
+canvas.pack()
+
+window.mainloop()
